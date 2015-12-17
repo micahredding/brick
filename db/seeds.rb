20be265adc8d898438b5ae880ba4f176a2ba14b9
Micah Redding <micahtredding@gmail.com>
2015-12-17 16:05:00 -0600

    Switch to using postgres

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,8 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
+
+Podcast.create(:title => "The Christian Transhumanist Podcast",       :path => "christiantranshumanist", :body =>"", :author => "", :keywords => "")
+Podcast.create(:title => "Startup Sequence",                          :path => "startup",                :body =>"", :author => "", :keywords => "")
+Podcast.create(:title => "The Podcast of Unnecessary Abstraction(s)", :path => "abstraction",            :body =>"", :author => "", :keywords => "")
+Podcast.create(:title => "Broadcast From The Singularity",            :path => "singularity",            :body =>"", :author => "", :keywords => "")
