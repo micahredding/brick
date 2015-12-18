0b9feba95ebaab0ce862f82d9d9908bfb66f4052
Micah Redding <micahtredding@gmail.com>
2015-12-17 18:49:37 -0600

    Permitted parameters

--- app/admin/episode.rb
+++ app/admin/episode.rb
@@ -3,7 +3,7 @@ ActiveAdmin.register Episode do
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
-# permit_params :list, :of, :attributes, :on, :model
+permit_params :title, :body, :number, :podcast_id
 #
 # or
 #
--- app/models/episode.rb
+++ app/models/episode.rb
@@ -1,6 +1,5 @@
 class Episode < ActiveRecord::Base
   belongs_to :podcast
-  attr_accessible :title, :number, :podcast_id
 
   def summary
     body ? body.truncate(255) : ""
