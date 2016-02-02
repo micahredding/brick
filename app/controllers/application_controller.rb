6d45785a557589ae28c42f2097bff706db6ff430
Micah Redding <micahtredding@gmail.com>
2016-02-01 20:45:59 -0600

    Converting all social settings into variables

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,11 +6,15 @@ class ApplicationController < ActionController::Base
   before_filter :set_meta_tags, :only => [:index, :show]
 
   def set_meta_tags
-    @meta_sitename    = Brick::SITETITLE
-    @meta_url         = request.original_url
-    @meta_type        = "blog"
-    @meta_title       = @meta_sitename
-    @meta_image       = "http://res.cloudinary.com/micahredding/image/upload/v1453084422/micah_instagram_500x500_ulcvcv.jpg"
-    @meta_description = @meta_sitename
+    @meta_sitename        = Brick::SITETITLE
+    @meta_url             = request.original_url
+    @meta_type            = "blog"
+    @meta_title           = @meta_sitename
+    @meta_image           = "http://res.cloudinary.com/micahredding/image/upload/v1453084422/micah_instagram_500x500_ulcvcv.jpg"
+    @meta_description     = @meta_sitename
+    @meta_fb_admins       = "510967360"
+    @meta_fb_app_id       = "1247388721942581"
+    @meta_twitter_site    = "@micahtredding"
+    @meta_twitter_creator = "@micahtredding"
   end
 end
