6e13444dcf6e2f0bd0a83704bd0580db479c3371
Micah Redding <micahtredding@gmail.com>
2016-02-03 22:51:25 -0600

    Add state to permitted params

--- app/admin/episode.rb
+++ app/admin/episode.rb
@@ -4,7 +4,7 @@ ActiveAdmin.register Episode do
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
-permit_params :podcast_id, :number, :title, :body, :recorded_at, :published_at, :image, :media, :media_length, :media_size, :summary, :published
+permit_params :podcast_id, :number, :title, :body, :recorded_at, :published_at, :image, :media, :media_length, :media_size, :summary, :published, :state
 #
 # or
 #
